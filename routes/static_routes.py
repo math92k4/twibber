750af42961e2e9646559dfc84808eda6960113da
Mathias <math92k4@stud.kea.dk>
2022-04-20 14:35:03 +0200

    test

--- routes/static_routes.py
+++ routes/static_routes.py
@@ -4,7 +4,6 @@ from bottle import get, static_file
 
 @get("/assets/<dir_name>/<file_name>")
 def _(dir_name, file_name):
-    print(f"./assets/{dir_name}")
     return static_file(file_name, root=f"./assets/{dir_name}")
 
 
@@ -12,6 +11,4 @@ def _(dir_name, file_name):
 
 @get("/assets/icons/<dir_name>/<file_name>")
 def _(dir_name, file_name):
-    print("#"*30)
-    print(f"./assets/{dir_name}")
     return static_file(file_name, root=f"./assets/icons{dir_name}")
